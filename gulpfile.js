dc91fb3df3bbe02d3b96d15741573b00edcb0369
Borodin Eduard <borodin.dev@gmail.com>
2018-11-13 13:58:09 +0200

    test project

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+// Подключаем Gulp
+var gulp = require("gulp");
+// Подключаем плагины Gulp
+var sass = require("gulp-sass"), // переводит SASS в CSS
+    cssnano = require("gulp-cssnano"), // Минимизация CSS
+    autoprefixer = require('gulp-autoprefixer'), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
+    imagemin = require('gulp-imagemin'), // Сжатие изображений
+    concat = require("gulp-concat"), // Объединение файлов - конкатенация
+    uglify = require("gulp-uglify"), // Минимизация javascript
+    rename = require("gulp-rename"), // Переименование файлов
+    browserSync = require('browser-sync');
+
+
+// Создаем простой таск
+gulp.task('myFirstTask', function() {
+    console.log('Привет, я твой первый таск!');
+});
+
+// Запуск тасков по умолчанию
+gulp.task("default", ["myFirstTask"]);
+
+
+// Копирование файлов HTML в папку dist
+gulp.task("fonts", function() {
+    return gulp.src("src/fonts/**/*.+(eot|otf|ttf|woff|woff2)")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("icons", function() {
+    return gulp.src("src/fonts/font-awesome-4.7.0/fonts/fontawesome-webfont.*")
+        .pipe(gulp.dest("dist/fonts/fonts"));
+});
+
+// Копирование файлов HTML в папку dist
+gulp.task("html", function() {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"));
+});
+
+// Объединение, компиляция Sass в CSS, простановка венд. префиксов и дальнейшая минимизация кода
+gulp.task("sass", function() {
+    return gulp.src(['src/scss/*.scss', ])
+        .pipe(concat('styles.scss'))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream(true));
+});
+
+// Объединение и сжатие JS-файлов
+gulp.task("scripts", function() {
+    return gulp.src([
+        "node_modules/jquery/dist/jquery.js",
+        "node_modules/bootstrap/dist/js/bootstrap.js",
+        "src/js/owl.carousel.min.js",
+        "node_modules/scrollwatch/dist/ScrollWatch-2.0.1.js",
+        "src/js/*.js"]) // директория откуда брать исходники
+        .pipe(concat('scripts.js')) // объеденим все js-файлы в один
+        .pipe(uglify()) // вызов плагина uglify - сжатие кода
+        .pipe(rename({ suffix: '.min' })) // вызов плагина rename - переименование файла с приставкой .min
+        .pipe(gulp.dest("dist/js")); // директория продакшена, т.е. куда сложить готовый файл
+});
+
+// Сжимаем картинки
+gulp.task('imgs', function() {
+    return gulp.src("src/images/**/*.+(jpg|jpeg|png|gif)")
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true
+        }))
+        .pipe(gulp.dest("dist/images"))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: ["./dist"]
+        },
+        notify: false,
+        browser: "firefox",
+
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Задача слежения за измененными файлами
+gulp.task("watch", function() {
+    gulp.watch("src/*.html", ["html"]);
+    gulp.watch("src/js/*.js", ["scripts"]);
+    gulp.watch("src/scss/*.scss", ["sass"]);
+    gulp.watch("src/images/*.+(jpg|jpeg|png|gif)", ["imgs"]);
+});
+
+// Запуск тасков по умолчанию
+gulp.task("default", ["html", "sass", "scripts", "imgs", "fonts", "icons", "watch", "browser-sync"]);
---
+++ src/js/scripts.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    (function() {
+        var swInstance = new ScrollWatch({});
+    })();
+
+    /* iPhone Slider */
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 0,
+        items: 2,
+        margin:15,
+        loop:true,
+        center: true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+});
+
+function Slider(slider_class, animation_type, width) {
+    var _ = this;
+    _.slider_tag = $('.' + slider_class);
+    _.slider_content = $(_.slider_tag).find('.slide-content');
+    _.prev = $(_.slider_tag).find('.prev');
+    _.next = $(_.slider_tag).find('.next');
+    _.cursor = 0;
+    $(_.prev).click(function () {
+        if(_.cursor > 0) {
+            _.cursor--;
+            $(_.slider_content).animate({
+                left: _.cursor*-width
+            }, 500, animation_type, function(){
+                if(_.cursor === 0) $(_.prev).addClass('grey-arrow');
+
+                // when you click the prev arrow, the next arrow should be grey
+                $(_.next).removeClass('grey-arrow');
+            });
+        }
+    });
+    $(_.next).click(function () {
+        if(_.cursor < 3) {
+            _.cursor++;
+            $(_.slider_content).animate({
+                left: _.cursor*-width
+            }, 500, animation_type, function(){
+                if(_.cursor === 3) $(_.next).addClass('grey-arrow');
+
+                // when you click the next arrow, the prev arrow should be grey
+                $(_.prev).removeClass('grey-arrow');
+            });
+        }
+    });
+}
+if ($(window).width() >= 320 && $(window).width() < 479.98) {
+    console.log('320');
+    new Slider('container', 'linear', 153);
+} else if ($(window).width() >= 480  && $(window).width() < 767.98){
+    console.log('480');
+    new Slider('container', 'linear', 222);
+} else if ($(window).width() >= 768){
+    console.log('768');
+    new Slider('container', 'linear', 365);
+} else {
+    console.log('God Damn what is the resolution of your monitor???');
+}
